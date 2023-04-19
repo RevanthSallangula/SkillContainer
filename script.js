660ec46c12e04c0ed9e4fe96465da8c08cef5d91
RevanthSallangula <theblazecode@gmail.com>
2023-04-19 09:42:08 +0530

    Fiddled round with the log in box
    Changed the Sign In button colours and moved the login box from opacity based to Toggle Show/Hide based

--- script.js
+++ script.js
@@ -4,12 +4,22 @@ const btnAbout = document.querySelector(".about-btn");
 const btnContact = document.querySelector(".contact-btn");
 const btnLoginRegiter = document.querySelector(".login-registration-btn");
 const btnLoginSignIn = document.querySelector(".login-sign-in-btn");
+const loginForm = document.querySelector(".login-form");
+loginForm.style.display = "none";
 const loggedIn = false;
 
 btnDashboard.addEventListener("click", function () {
   if (loggedIn === false) {
-    document.querySelector(".login-form").style.opacity = 1;
+    console.log("Not Logged In");
   } else {
     console.log("Already Logged In");
   }
 });
+
+function toggleShow() {
+  if (loginForm.style.display === "none") {
+    loginForm.style.display = "flex";
+  } else {
+    loginForm.style.display = "none";
+  }
+}
