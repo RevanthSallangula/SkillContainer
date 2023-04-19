437dcff2c43cd4a5fcbaa863e01f619501b76682
RevanthSallangula <theblazecode@gmail.com>
2023-04-19 09:17:02 +0530

    Day 2-1

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
-document
-  .querySelector(".dashboard-btn")
-  .addEventListener("click", function () {});
-document
-  .querySelector(".pricing-btn")
-  .addEventListener("click", function () {});
-document.querySelector(".about-btn").addEventListener("click", function () {});
-document
-  .querySelector(".contact-btn")
-  .addEventListener("click", function () {});
+const btnDashboard = document.querySelector(".dashboard-btn");
+const btnPricing = document.querySelector(".pricing-btn");
+const btnAbout = document.querySelector(".about-btn");
+const btnContact = document.querySelector(".contact-btn");
+const btnLoginRegiter = document.querySelector(".login-registration-btn");
+const btnLoginSignIn = document.querySelector(".login-sign-in-btn");
+const loggedIn = false;
+
+btnDashboard.addEventListener("click", function () {
+  if (loggedIn === false) {
+    document.querySelector(".login-form").style.opacity = 1;
+  } else {
+    console.log("Already Logged In");
+  }
+});
