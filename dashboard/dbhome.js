3b185bda22cb26f34287fd88ff6013200421f02e
suansh25 <kurakulasuansh@gmail.com>
2023-04-28 21:58:59 +0530

    update

--- dashboard/dbhome.js
+++ dashboard/dbhome.js
@@ -13,6 +13,7 @@ const btnUser = document.querySelector(".navlist-btn-user");
 const btnCertificates = document.querySelector(".navlist-btn-certificates");
 const btnContainers = document.querySelector(".navlist-btn-containers");
 const btnSettings = document.querySelector(".navlist-btn-settings");
+const certificateGrid=document.querySelector("#certificate-grid")
 
 //changing menu buttons
 function menuBtnChange() {
@@ -74,4 +75,9 @@ function searchToggle(obj, evt) {
     container.find(".search-input").val("");
   }
 }
-// 
+// Array to hold certificate elements
+certificateElements = [12,37,3478,238,11];
+
+for (i of certificateElements){
+  certificateGrid.insertAdjacentHTML("beforeEnd",`<div class=\'item item-${i}\'>${i}</div>`)
+}
