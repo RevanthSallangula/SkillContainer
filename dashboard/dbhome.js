8b8fef156413360311ea05917463544f5fd12b57
RevanthSallangula <theblazecode@gmail.com>
2023-07-20 20:22:26 +0530

    Initialized npm

--- dashboard/dbhome.js
+++ dashboard/dbhome.js
@@ -17,138 +17,138 @@ const certificateGrid = document.querySelector("#certificate-grid");
 const certificateCard = document.querySelectorAll(".certificate-card");
 const containerGrid = document.querySelector("#container-grid");
 const searchInputCertificate = document.querySelector(
-  ".search-input-certificate"
+    ".search-input-certificate"
 );
 const searchInputContainer = document.querySelector(".search-input-container");
 
 //changing menu buttons
 function menuBtnChange() {
-  if (sidebar.classList.contains("open")) {
-    closeBtn.classList.replace("bx-menu", "bx-menu-alt-right");
-  } else {
-    closeBtn.classList.replace("bx-menu-alt-right", "bx-menu");
-  }
+    if (sidebar.classList.contains("open")) {
+        closeBtn.classList.replace("bx-menu", "bx-menu-alt-right");
+    } else {
+        closeBtn.classList.replace("bx-menu-alt-right", "bx-menu");
+    }
 }
 
 closeBtn.addEventListener("click", () => {
-  sidebar.classList.toggle("open");
-  menuBtnChange();
+    sidebar.classList.toggle("open");
+    menuBtnChange();
 });
 
 searchBtn.addEventListener("click", () => {
-  // Sidebar open when you click on the search iocn
-  sidebar.classList.toggle("open");
-  menuBtnChange();
+    // Sidebar open when you click on the search iocn
+    sidebar.classList.toggle("open");
+    menuBtnChange();
 });
 
 //close all active tabs
 function closeAllTabs() {
-  displayCertificates.style.display = "none";
-  displayContainers.style.display = "none";
-  displayDashboard.style.display = "none";
-  displaySettings.style.display = "none";
-  displayUser.style.display = "none";
+    displayCertificates.style.display = "none";
+    displayContainers.style.display = "none";
+    displayDashboard.style.display = "none";
+    displaySettings.style.display = "none";
+    displayUser.style.display = "none";
 }
 
 //displays current tab
 function displayCurrentTab(name) {
-  closeAllTabs();
-  if (sidebar.classList.contains("open")) {
-    sidebar.classList.toggle("open");
-    menuBtnChange();
-  }
-  document.querySelector(`.home-${name}`).style.display = "flex";
-  navTitle.textContent = name[0].toUpperCase() + name.slice(1);
+    closeAllTabs();
+    if (sidebar.classList.contains("open")) {
+        sidebar.classList.toggle("open");
+        menuBtnChange();
+    }
+    document.querySelector(`.home-${name}`).style.display = "flex";
+    navTitle.textContent = name[0].toUpperCase() + name.slice(1);
 }
 
 //Logs Out to index.html
 function logOut() {
-  location.href = "/index.html";
+    location.href = "/index.html";
 }
 
 //Search Bar
 function searchToggle(obj, evt) {
-  var container = $(obj).closest(".search-wrapper");
-  if (!container.hasClass("active")) {
-    container.addClass("active");
-    evt.preventDefault();
-  } else if (
-    container.hasClass("active") &&
-    $(obj).closest(".input-holder").length == 0
-  ) {
-    container.removeClass("active");
-    // clear input
-    container.find(".search-input").val("");
-    //Code to Display all cards of certificates and containers after closing the search input
-    for (i in certificateElements) {
-      document.querySelector(
-        `#certificate-${certificateElements[i].index}`
-      ).style.display = "block";
+    var container = $(obj).closest(".search-wrapper");
+    if (!container.hasClass("active")) {
+        container.addClass("active");
+        evt.preventDefault();
+    } else if (
+        container.hasClass("active") &&
+        $(obj).closest(".input-holder").length == 0
+    ) {
+        container.removeClass("active");
+        // clear input
+        container.find(".search-input").val("");
+        //Code to Display all cards of certificates and containers after closing the search input
+        for (i in certificateElements) {
+            document.querySelector(
+                `#certificate-${certificateElements[i].index}`
+            ).style.display = "block";
+        }
+        for (i in containerElements) {
+            document.querySelector(
+                `#container-${containerElements[i].index}`
+            ).style.display = "block";
+        }
     }
-    for (i in containerElements) {
-      document.querySelector(
-        `#container-${containerElements[i].index}`
-      ).style.display = "block";
-    }
-  }
 }
 
 //Array to hold certificate elements and container elements
 // certificateElements acting as call from BE
 const certificateElements = [
-  {
-    index: 16,
-    title: "Introduction to Android Programming",
-    issueDate: "25-07-2021",
-    issuedBy: "Google",
-    description:
-      "Issued by Google to certify that the holder of this certificate is capable of handling introductory Android programming",
-    tags: ["Android", "Coding", "Google"],
-  },
-  {
-    index: 12,
-    title: "Programming Essentials",
-    issueDate: "05-01-2020",
-    issuedBy: "Udemy",
-    description:
-      "Issued by Udemy to certify that the holder of this certificate is capable of handling introductory programming concepts",
-    tags: ["Coding", "Udemy"],
-  },
-  {
-    index: 31,
-    title: "Introduction to AWS Cloud Services",
-    issueDate: "02-05-2023",
-    issuedBy: "Amazon",
-    description:
-      "Issued by Amazon to certify that the holder of this certificate has a solid understanding of the processes involved in the functioning of AWS Cloud",
-    tags: ["AWS", "Cloud", "Amazon"],
-  },
-  {
-    index: 42,
-    title: "JavaScript Essentials",
-    issueDate: "25-07-2021",
-    issuedBy: "Udemy",
-    description:
-      "Issued by Udemy to certify that the holder of this certificate is capable of handling introductory JavaScript programming",
-    tags: ["Coding", "JavaScript", "Udemy"],
-  },
-  {
-    index: 15,
-    title: "Beginner's Guide to Swift",
-    issueDate: "10-11-2022",
-    issuedBy: "Apple",
-    description:
-      "Issued by Apple to certify that the holder of this certificate is capable of handling introductory Swift programming",
-    tags: ["Coding", "Swift", "Apple"],
-  },
+    {
+        index: 16,
+        title: "Introduction to Android Programming",
+        issueDate: "25-07-2021",
+        issuedBy: "Google",
+        description:
+            "Issued by Google to certify that the holder of this certificate is capable of handling introductory Android programming",
+        tags: ["Android", "Coding", "Google"],
+    },
+    {
+        index: 12,
+        title: "Programming Essentials",
+        issueDate: "05-01-2020",
+        issuedBy: "Udemy",
+        description:
+            "Issued by Udemy to certify that the holder of this certificate is capable of handling introductory programming concepts",
+        tags: ["Coding", "Udemy"],
+    },
+    {
+        index: 31,
+        title: "Introduction to AWS Cloud Services",
+        issueDate: "02-05-2023",
+        issuedBy: "Amazon",
+        description:
+            "Issued by Amazon to certify that the holder of this certificate has a solid understanding of the processes involved in the functioning of AWS Cloud",
+        tags: ["AWS", "Cloud", "Amazon"],
+    },
+    {
+        index: 42,
+        title: "JavaScript Essentials",
+        issueDate: "25-07-2021",
+        issuedBy: "Udemy",
+        description:
+            "Issued by Udemy to certify that the holder of this certificate is capable of handling introductory JavaScript programming",
+        tags: ["Coding", "JavaScript", "Udemy"],
+    },
+    {
+        index: 15,
+        title: "Beginner's Guide to Swift",
+        issueDate: "10-11-2022",
+        issuedBy: "Apple",
+        description:
+            "Issued by Apple to certify that the holder of this certificate is capable of handling introductory Swift programming",
+        tags: ["Coding", "Swift", "Apple"],
+    },
 ];
 
 for (i of certificateElements) {
-  certificateGrid.insertAdjacentHTML(
-    "beforeEnd",
-    `<div id="certificate-${
-      i.index
-    }" class="certificate-card" style="display:block">
+    certificateGrid.insertAdjacentHTML(
+        "beforeEnd",
+        `<div id="certificate-${
+            i.index
+        }" class="certificate-card" style="display:block">
     <div class="badge">${i.issueDate}</div>
     <div class="certificate-image">
       <img src="images/demo.jpg" alt="" />
@@ -165,58 +165,60 @@ for (i of certificateElements) {
       </div>
     </div>
   </div>`
-  );
+    );
 }
 
 //Cerificate Search Functioning
 searchInputCertificate.addEventListener("keyup", function () {
-  if (searchInputCertificate.value != "") {
-    for (i in certificateElements) {
-      let certificateSearchElements = Object.values(certificateElements[`${i}`])
-        .flat()
-        .toString()
-        .toLowerCase()
-        .split(",");
-      document.querySelector(
-        `#certificate-${certificateElements[i].index}`
-      ).style.display = "none";
-      if (
-        certificateSearchElements.some((item) =>
-          item.includes(searchInputCertificate.value.toLowerCase())
-        )
-      ) {
-        document.querySelector(
-          `#certificate-${certificateElements[i].index}`
-        ).style.display = "block";
-      }
+    if (searchInputCertificate.value != "") {
+        for (i in certificateElements) {
+            let certificateSearchElements = Object.values(
+                certificateElements[`${i}`]
+            )
+                .flat()
+                .toString()
+                .toLowerCase()
+                .split(",");
+            document.querySelector(
+                `#certificate-${certificateElements[i].index}`
+            ).style.display = "none";
+            if (
+                certificateSearchElements.some((item) =>
+                    item.includes(searchInputCertificate.value.toLowerCase())
+                )
+            ) {
+                document.querySelector(
+                    `#certificate-${certificateElements[i].index}`
+                ).style.display = "block";
+            }
+        }
+        //Else part meant to show display of All blocks
+        //But the code somehow works without the 'else' part
+        //Delete if you want to save some space
+    } else {
+        for (i in certificateElements) {
+            document.querySelector(
+                `#certificate-${certificateElements[i].index}`
+            ).style.display = "block";
+        }
     }
-    //Else part meant to show display of All blocks
-    //But the code somehow works without the 'else' part
-    //Delete if you want to save some space
-  } else {
-    for (i in certificateElements) {
-      document.querySelector(
-        `#certificate-${certificateElements[i].index}`
-      ).style.display = "block";
-    }
-  }
 });
 
 containerElements = [
-  { index: 1, title: "Udemy Certificates", count: 7, date: "12-09-2018" },
-  { index: 2, title: "Google Certificates", count: 12, date: "10-11-2018" },
-  { index: 3, title: "Amazon Certificates", count: 4, date: "19-01-2012" },
-  { index: 4, title: "Apple Certificates", count: 10, date: "10-01-2022" },
-  { index: 5, title: "Meta Certificates", count: 12, date: "10-01-2022" },
-  { index: 6, title: "Coursera Certificates", count: 16, date: "10-01-2022" },
-  { index: 7, title: "FCC Certificates", count: 7, date: "10-01-2022" },
-  { index: 8, title: "KA Certificates", count: 2, date: "10-01-2022" },
+    { index: 1, title: "Udemy Certificates", count: 7, date: "12-09-2018" },
+    { index: 2, title: "Google Certificates", count: 12, date: "10-11-2018" },
+    { index: 3, title: "Amazon Certificates", count: 4, date: "19-01-2012" },
+    { index: 4, title: "Apple Certificates", count: 10, date: "10-01-2022" },
+    { index: 5, title: "Meta Certificates", count: 12, date: "10-01-2022" },
+    { index: 6, title: "Coursera Certificates", count: 16, date: "10-01-2022" },
+    { index: 7, title: "FCC Certificates", count: 7, date: "10-01-2022" },
+    { index: 8, title: "KA Certificates", count: 2, date: "10-01-2022" },
 ];
 
 for (i of containerElements) {
-  containerGrid.insertAdjacentHTML(
-    "beforeEnd",
-    `<div id="container-${i.index}" class="container-card split" style="display:block">
+    containerGrid.insertAdjacentHTML(
+        "beforeEnd",
+        `<div id="container-${i.index}" class="container-card split" style="display:block">
     <div class="container-image">
       <img src="images/demo.jpg" alt="" />
     </div>
@@ -227,38 +229,40 @@ for (i of containerElements) {
     </div>
   </div>
 `
-  );
+    );
 }
 
 searchInputContainer.addEventListener("keyup", function () {
-  if (searchInputContainer.value != "") {
-    for (i in containerElements) {
-      let containerSearchElements = Object.values(containerElements[`${i}`])
-        .flat()
-        .toString()
-        .toLowerCase()
-        .split(",");
-      document.querySelector(
-        `#container-${containerElements[i].index}`
-      ).style.display = "none";
-      if (
-        containerSearchElements.some((item) =>
-          item.includes(searchInputContainer.value.toLowerCase())
-        )
-      ) {
-        document.querySelector(
-          `#container-${containerElements[i].index}`
-        ).style.display = "block";
-      }
+    if (searchInputContainer.value != "") {
+        for (i in containerElements) {
+            let containerSearchElements = Object.values(
+                containerElements[`${i}`]
+            )
+                .flat()
+                .toString()
+                .toLowerCase()
+                .split(",");
+            document.querySelector(
+                `#container-${containerElements[i].index}`
+            ).style.display = "none";
+            if (
+                containerSearchElements.some((item) =>
+                    item.includes(searchInputContainer.value.toLowerCase())
+                )
+            ) {
+                document.querySelector(
+                    `#container-${containerElements[i].index}`
+                ).style.display = "block";
+            }
+        }
+        //Else part meant to show display of All blocks
+        //But the code somehow works without the 'else' part
+        //Delete if you want to save some space
+    } else {
+        for (i in containerElements) {
+            document.querySelector(
+                `#container-${containerElements[i].index}`
+            ).style.display = "block";
+        }
     }
-    //Else part meant to show display of All blocks
-    //But the code somehow works without the 'else' part
-    //Delete if you want to save some space
-  } else {
-    for (i in containerElements) {
-      document.querySelector(
-        `#container-${containerElements[i].index}`
-      ).style.display = "block";
-    }
-  }
 });
--- script.js
+++ script.js
@@ -8,45 +8,45 @@ const loginForm = document.querySelector(".login-form");
 const registrationForm = document.querySelector(".registration-form");
 const contactForm = document.querySelector(".contact-form");
 const btnRegistrationSignUp = document.querySelector(
-  ".registration-sign-up-btn"
+    ".registration-sign-up-btn"
 );
 
 const loggedIn = false;
 
 function closeAll() {
-  loginForm.style.display = "none";
-  registrationForm.style.display = "none";
-  contactForm.style.display = "none";
+    loginForm.style.display = "none";
+    registrationForm.style.display = "none";
+    contactForm.style.display = "none";
 }
 
 closeAll();
 
 function toggleShowLogin() {
-  closeAll();
-  if (loginForm.style.display === "none" && loggedIn === false) {
-    loginForm.style.display = "flex";
-  }
+    closeAll();
+    if (loginForm.style.display === "none" && loggedIn === false) {
+        loginForm.style.display = "flex";
+    }
 }
 
 function toggleShowRegistration() {
-  closeAll();
-  if (registrationForm.style.display === "none") {
-    registrationForm.style.display = "flex";
-  }
+    closeAll();
+    if (registrationForm.style.display === "none") {
+        registrationForm.style.display = "flex";
+    }
 }
 
 function toggleShowContact() {
-  closeAll();
-  if (contactForm.style.display === "none") {
-    contactForm.style.display = "flex";
-  }
+    closeAll();
+    if (contactForm.style.display === "none") {
+        contactForm.style.display = "flex";
+    }
 }
 
 btnDashboard.addEventListener("click", function () {
-  if (loggedIn === false) {
-    console.log("Not Logged In");
-  } else {
-    console.log("Already Logged In");
-    location.href = "/dashboard/dashboard.html";
-  }
+    if (loggedIn === false) {
+        console.log("Not Logged In");
+    } else {
+        console.log("Already Logged In");
+        location.href = "/dashboard/dashboard.html";
+    }
 });
