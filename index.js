ecc9d93aa6b570002d4553f9fd385ed5ec565a46
RevanthSallangula <theblazecode@gmail.com>
2023-07-21 13:30:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Mained in package.json
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+    res.sendFile(
+        "C:/Users/Revanth Sallangula/Downloads/SkillContainer/LandingPage/index.html"
+    );
+});
+
+app.listen(port, () => {
+    console.log(`Server at ${port}`);
+});
