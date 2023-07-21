8d1cc4372fc4a4c8c4b126a1f99e5f838a19ef72
RevanthSallangula <theblazecode@gmail.com>
2023-07-21 16:38:02 +0530

    Update

--- index.js
+++ index.js
@@ -1,37 +1,26 @@
 // Mained in package.json
 import express from "express";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
-const port = 3000;
+const port = 4000;
 
-app.use(
-    express.static(
-        "C:/Users/Revanth Sallangula/Downloads/SkillContainer/LandingPage"
-    )
-);
-app.use(
-    express.static(
-        "C:/Users/Revanth Sallangula/Downloads/SkillContainer/dashboard"
-    )
-);
+app.use(express.static(`${__dirname}/LandingPage/`));
+app.use(express.static(`${__dirname}/dashboard/`));
+app.use(express.static(`${__dirname}/dashboard/images/`));
 
-app.use(
-    express.static(
-        "C:/Users/Revanth Sallangula/Downloads/SkillContainer/dashboard/images"
-    )
-);
 app.get("/", (req, res) => {
-    res.sendFile(
-        "C:/Users/Revanth Sallangula/Downloads/SkillContainer/LandingPage/index.html"
-    );
+    res.sendFile(`${__dirname}/LandingPage/index.html`);
 });
 
-app.get("/dashboard", (req, res) => {
-    res.sendFile(
-        "C:/Users/Revanth Sallangula/Downloads/SkillContainer/dashboard/dashboard.html"
-    );
+app.get("/dashboard/", (req, res) => {
+    res.sendFile(`${__dirname}/dashboard/dashboard.html`);
 });
 
 app.listen(port, () => {
     console.log(`Server at ${port}`);
+    console.log("Current directory:", __dirname);
 });
